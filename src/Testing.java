5ce6e3f32615bf7e04623cf5c65827c28877e7fe
Yen-Chi Ho <yenchiho@gmail.com>
2012-10-18 10:14:05 +0800

    New Stuff Here
    See if it works

---
+++ src/Testing.java
@@ -0,0 +1,20 @@
+
+public class Testing {
+	private  static final  int TEAM= 20;
+	public static void main(String [ ] args){
+		System.out.println("GitHub with eclipse egit is cool");
+		System.out.println("I didn't use google host because github is more easier to use");
+		askingQuestion();
+		successMsg();
+	}
+	 
+	public static void askingQuestion(){
+		  System.out.println("Why still cannot connect to github but show successful in eclipse");
+	  }
+	
+	public static void successMsg(){
+		System.out.println("Finally success connect github with eclipse,yeah");
+	}
+}
+
+ 
