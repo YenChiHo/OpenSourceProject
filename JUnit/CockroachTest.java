c049820326b8d22d65abb2e6bba15f7e49892e88
unknown <Huei@huei-pc.(none)>
2012-10-24 22:55:56 +0800

    so strange @@

---
+++ JUnit/CockroachTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CockroachTest {
+
+	
+	
+	
+	@Test
+	public void testCurrentStatus() {
+		Cockroach cockroach=new Cockroach();
+		cockroach.currentStatus();
+	}
+
+}
