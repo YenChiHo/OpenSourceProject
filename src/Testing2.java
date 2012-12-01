0a3969fae1135c55c58f1ad8fe85e2362db01504
Yen-Chi Ho <yenchiho@gmail.com>
2012-12-01 13:36:02 +0800

    Try to see if I wrote the Wiki in a correct way
    Gosh I've tried so many times and it still fails...

---
+++ src/Testing2.java
@@ -0,0 +1,25 @@
+
+public class Testing {
+	private  static final  int TEAM= 20;
+	public static void main(String [ ] args){
+		System.out.println("GitHub with eclipse egit is cool");
+		System.out.println("I didn't use google host because github is more easier to use");
+        System.out.println("I've done my WIKI as required but it didn't show for some reason =.= ");
+		askingQuestion();
+		successMsg();
+	}
+	 
+	public static void askingQuestion(){
+		  System.out.println("Why still cannot connect to github but show successful in eclipse");
+	  }
+	
+	public static void successMsg(){
+		System.out.println("Finally success connect github with eclipse,yeah");
+	}
+    public class Hello{
+        system.out.println("Just wanna see if it's okay!")
+    }
+    
+}
+
+ 
