3fcf0248c3e0ae284f739678717a07d12269c152
Yen-Chi Ho <yenchiho@gmail.com>
2013-01-17 10:05:15 +0800

    Update Slides for this project
    Final Project Slides

--- src/gpstracking/OpenGPSTracker/test/src/nl/sogeti/android/gpstracker/tests/perf/LoggerStressTest.java
+++ src/gpstracking/OpenGPSTracker/test/src/nl/sogeti/android/gpstracker/tests/perf/LoggerStressTest.java
@@ -1,31 +1,3 @@
-/*------------------------------------------------------------------------------
- **     Ident: Innovation en Inspiration > Google Android 
- **    Author: rene
- ** Copyright: (c) Jan 22, 2009 Sogeti Nederland B.V. All Rights Reserved.
- **------------------------------------------------------------------------------
- ** Sogeti Nederland B.V.            |  No part of this file may be reproduced  
- ** Distributed Software Engineering |  or transmitted in any form or by any        
- ** Lange Dreef 17                   |  means, electronic or mechanical, for the      
- ** 4131 NJ Vianen                   |  purpose, without the express written    
- ** The Netherlands                  |  permission of the copyright holder.
- *------------------------------------------------------------------------------
- *
- *   This file is part of OpenGPSTracker.
- *
- *   OpenGPSTracker is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   OpenGPSTracker is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with OpenGPSTracker.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
 package nl.sogeti.android.gpstracker.tests.perf;
 
 import nl.sogeti.android.gpstracker.tests.R;
@@ -36,12 +8,6 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.test.PerformanceTestCase;
 import android.test.suitebuilder.annotation.LargeTest;
 
-/**
- * Goal is to feed as the LoggerMap as many points as possible to give it a good workout.
- *
- * @version $Id$
- * @author rene (c) Mar 15, 2009, Sogeti B.V.
- */
 public class LoggerStressTest extends ActivityInstrumentationTestCase2<CommonLoggerMap> implements PerformanceTestCase
 {
    private static final Class<CommonLoggerMap> CLASS = CommonLoggerMap.class;
--- src/gpstracking/OpenGPSTracker/test/src/nl/sogeti/android/gpstracker/tests/perf/MapStressTest.java
+++ src/gpstracking/OpenGPSTracker/test/src/nl/sogeti/android/gpstracker/tests/perf/MapStressTest.java
@@ -1,31 +1,3 @@
-/*------------------------------------------------------------------------------
- **     Ident: Innovation en Inspiration > Google Android 
- **    Author: rene
- ** Copyright: (c) Jan 22, 2009 Sogeti Nederland B.V. All Rights Reserved.
- **------------------------------------------------------------------------------
- ** Sogeti Nederland B.V.            |  No part of this file may be reproduced  
- ** Distributed Software Engineering |  or transmitted in any form or by any        
- ** Lange Dreef 17                   |  means, electronic or mechanical, for the      
- ** 4131 NJ Vianen                   |  purpose, without the express written    
- ** The Netherlands                  |  permission of the copyright holder.
- *------------------------------------------------------------------------------
- *
- *   This file is part of OpenGPSTracker.
- *
- *   OpenGPSTracker is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   OpenGPSTracker is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with OpenGPSTracker.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
 package nl.sogeti.android.gpstracker.tests.perf;
 
 import java.io.FileReader;
@@ -49,12 +21,7 @@ import android.test.PerformanceTestCase;
 import android.test.suitebuilder.annotation.LargeTest;
 import android.util.Log;
 
-/**
- * Goal is to feed as the LoggerMap as many points as possible to give it a good workout.
- * 
- * @version $Id: LoggerMapStressTest.java 47 2009-05-17 19:15:00Z rcgroot $
- * @author rene (c) Mar 15, 2009, Sogeti B.V.
- */
+
 public class MapStressTest extends ActivityInstrumentationTestCase2<CommonLoggerMap> implements PerformanceTestCase
 {
    private static final Class<CommonLoggerMap> CLASS = CommonLoggerMap.class;
